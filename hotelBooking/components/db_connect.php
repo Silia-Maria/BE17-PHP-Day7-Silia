147aac8d3f3efc4e18c0eb8b67cae8b457bbeb6d
Silia Cronauer <silia.maria@web.de>
2022-11-16 11:46:58 +0100

    added connection

---
+++ hotelBooking/components/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+
+$hostname = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "hotelbooking";
+
+try {
+    $connect = mysqli_connect($hostname, $username, $password, $dbname);
+    echo "connected";
+} catch (Exception $e) {
+    echo "error" . mysqli_connect_error();
+}
+
+function var_dump_pretty($var)
+{
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+}
